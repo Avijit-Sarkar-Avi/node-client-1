5e376e6fa82f4243a1df499e0fb353674e6f88c5
Avijit-Sarkar-Avi <avijit215054@gmail.com>
2022-10-29 12:14:17 +0600

    set useState and useEffect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/users')
+      .then(res => res.json())
+      .then(data => setUsers(data));
+  }, [])
   return (
     <div className="App">
-
+      <h2>Users: {users.length}</h2>
     </div>
   );
 }
