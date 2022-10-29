ed54a20ef23e8ee579f6fc28a9f8a78184312b50
Avijit-Sarkar-Avi <avijit215054@gmail.com>
2022-10-29 12:25:53 +0600

    create a form and create an onSubmit button and event handler

--- src/App.js
+++ src/App.js
@@ -9,8 +9,23 @@ function App() {
       .then(res => res.json())
       .then(data => setUsers(data));
   }, [])
+
+  const handleAddUser = event => {
+    event.preventDefault();
+
+  }
   return (
     <div className="App">
+
+      <form onSubmit={handleAddUser}>
+        <input type="text" name='name' />
+        <br />
+        <input type="email" name='email' />
+        <br />
+        <button type='submit'>Add User</button>
+      </form>
+
+
       <h2>Users: {users.length}</h2>
 
       {
